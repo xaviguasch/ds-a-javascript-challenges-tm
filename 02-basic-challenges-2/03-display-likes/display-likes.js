20872482b0084533c6a45b2ae6bc69a0e4d1c5bc
Xavi Guasch <xaviguasch@gmail.com>
2023-09-10 15:21:06 +0200

    added solution for 13-display likes and 14-find-missing-number challenges

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,15 @@
-function displayLikes() {}
+function displayLikes(peopleArr) {
+  if (peopleArr.length === 0) {
+    return 'no one likes this'
+  } else if (peopleArr.length === 1) {
+    return `${peopleArr[0]} likes this`
+  } else if (peopleArr.length === 2) {
+    return `${peopleArr[0]} and ${peopleArr[1]} like this`
+  } else if (peopleArr.length === 3) {
+    return `${peopleArr[0]}, ${peopleArr[1]} and ${peopleArr[2]} like this`
+  } else {
+    return `${peopleArr[0]}, ${peopleArr[1]} and ${peopleArr.length - 2} others like this`
+  }
+}
 
-module.exports = displayLikes;
+module.exports = displayLikes
--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,13 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  if (arr.length === 0) return 1
 
-module.exports = findMissingNumber;
+  const n = arr.length + 1
+
+  const sumOfNums = (n * (n + 1)) / 2 // same as adding 1 + 2 + 3... to 10
+
+  const sumOfArrNums = arr.reduce((acc, currV) => acc + currV, 0)
+
+  return sumOfNums - sumOfArrNums
+}
+
+module.exports = findMissingNumber
