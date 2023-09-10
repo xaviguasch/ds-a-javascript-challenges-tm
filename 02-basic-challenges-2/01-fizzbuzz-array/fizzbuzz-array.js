37b958cc0704d82d8935cf71d476832d8fa2c2e7
Xavi Guasch <xaviguasch@gmail.com>
2023-09-10 14:01:57 +0200

    added solution for 11-fizzbuzz challenge

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,19 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  const newArr = []
 
-module.exports = fizzBuzzArray;
+  for (let i = 1; i <= num; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      newArr.push('FizzBuzz')
+    } else if (i % 3 === 0) {
+      newArr.push('Fizz')
+    } else if (i % 5 === 0) {
+      newArr.push('Buzz')
+    } else {
+      newArr.push(i)
+    }
+  }
+
+  return newArr
+}
+
+module.exports = fizzBuzzArray
