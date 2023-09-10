1510d231e07b71f564af74227165b0208810befc
Xavi Guasch <xaviguasch@gmail.com>
2023-09-10 14:59:12 +0200

    added solution for 12-array-intersection challenge

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,16 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const interArr = []
 
-module.exports = arrayIntersection;
+  arr1.forEach((num) => {
+    if (arr2.includes(num)) {
+      // we want to avoid duplicates in the final array so we check it first
+      if (!interArr.includes(num)) {
+        interArr.push(num)
+      }
+    }
+  })
+
+  return interArr
+}
+
+module.exports = arrayIntersection
