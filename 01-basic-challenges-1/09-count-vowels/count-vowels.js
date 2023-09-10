05abc302afce0f7eb8f6dd3730c624a9b73c08d0
Xavi Guasch <xaviguasch@gmail.com>
2023-09-10 13:24:40 +0200

    added solution 09-count-vowels in 01-basic-challenges

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,20 @@
-function countVowels() {}
+function countVowels(str) {
+  const splittedStr = str.toLowerCase().split('')
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  let vowelCount = 0
 
-module.exports = countVowels;
+  splittedStr.forEach((w) => {
+    // if (vowels.includes(w)) {
+    //   vowelCount++
+    // }
+
+    // ALTERNATIVE SOLUTION
+    if (vowels.indexOf(w) !== -1) {
+      vowelCount++
+    }
+  })
+
+  return vowelCount
+}
+
+module.exports = countVowels
