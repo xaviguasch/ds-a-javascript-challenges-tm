3a750f5afbbc2625102d07bcccad5893575fec22
Xavi Guasch <xaviguasch@gmail.com>
2023-09-10 15:53:23 +0200

    added solution to 15-find-missing-letter challenge

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,53 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  if (arr.length === 0) return ''
 
-module.exports = findMissingLetter;
+  const lowerCaseArr = arr.map((letter) => letter.toLowerCase())
+
+  const alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ]
+
+  const idxOf1stLetter = alphabet.indexOf(lowerCaseArr[0])
+
+  let missingLetter = ''
+
+  for (let i = idxOf1stLetter; i < idxOf1stLetter + lowerCaseArr.length; i++) {
+    if (!lowerCaseArr.includes(alphabet[i])) {
+      missingLetter = alphabet[i]
+    }
+  }
+
+  // returns the letter to its original format, if necessary
+  if (arr[0] === arr[0].toUpperCase()) {
+    return missingLetter.toUpperCase()
+  } else {
+    return missingLetter
+  }
+}
+
+module.exports = findMissingLetter
