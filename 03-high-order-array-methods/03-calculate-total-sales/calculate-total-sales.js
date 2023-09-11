148afdbee952979da35ffd35a547eb1b98b45e46
Xavi Guasch <xaviguasch@gmail.com>
2023-09-11 16:48:51 +0200

    added solution to 22-calculate-total-sales challenge

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,14 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, tax) {
+  const totalSales = products.reduce(
+    (acc, currV) => acc + currV.price * currV.quantity,
+    0
+  )
 
-module.exports = calculateTotalSalesWithTax;
+  const taxAmount = (totalSales * tax) / 100
+
+  const totalSalesWithTax = totalSales + taxAmount
+
+  return parseFloat(totalSalesWithTax.toFixed(2))
+}
+
+module.exports = calculateTotalSalesWithTax
