9bde21860f4f19658e3b94ad8fd71d140dd64b33
Xavi Guasch <xaviguasch@gmail.com>
2023-09-11 15:27:48 +0200

    added solution to 20-validate-email challenge

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,32 @@
-function validateEmail() {}
+// SOLUTION WITH REGULAR EXPRESSION
+// function validateEmail(email) {
+//   const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
 
-module.exports = validateEmail;
+//   return emailRegex.test(email)
+// }
+
+function validateEmail(email) {
+  if (email.indexOf('@') === -1) {
+    return false
+  }
+
+  const [name, domain] = email.split('@')
+
+  if (domain.indexOf('.') === -1) {
+    return false
+  }
+
+  if (name.length === 0 || domain.length < 3) {
+    return false
+  }
+
+  const [domainName, domainExt] = domain.split('.')
+
+  if (domainName.length <= 1 && domainExt.length <= 1) {
+    return false
+  }
+
+  return true
+}
+
+module.exports = validateEmail
--- pending-to-revisit.md
+++ pending-to-revisit.md
@@ -1,2 +1,2 @@
 17-first-non-repeating FAILED AT FIRST TRY, TRY AGAIN!!!
-
+20-validate-email FAILED AT FIRST TRY, TRY AGAIN!!! both regex and normal solutions
