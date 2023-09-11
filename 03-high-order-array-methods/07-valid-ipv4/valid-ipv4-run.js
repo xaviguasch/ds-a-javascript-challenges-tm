e31147df9c8fd2e3e9edaec904742363cea0d003
Xavi Guasch <xaviguasch@gmail.com>
2023-09-11 19:32:50 +0200

    added solution to 26-valid-ipv4 challenge

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4-run.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4-run.js
@@ -1,6 +1,6 @@
-const isValidIPv4 = require('./valid-ipv4.js');
+const isValidIPv4 = require('./valid-ipv4.js')
 
-const result1 = isValidIPv4('122.164.23.21');
-const result2 = isValidIPv4('122.164.23.21.33');
+const result1 = isValidIPv4('122.164.23.21')
+const result2 = isValidIPv4('123.045.067.089')
 
-console.log(result1, result2);
+console.log(result1, result2)
--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,26 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (address) => {
+  const octetsArr = address.split('.')
 
-module.exports = isValidIPv4;
+  if (octetsArr.length !== 4) {
+    return false
+  }
+  // checks for leading zeros, if there's
+  // one octet with one, whole thing is false
+  if (octetsArr.some((octet) => /^0+/.test(octet))) {
+    return false
+  }
+
+  let isValid = true
+
+  octetsArr.forEach((oct) => {
+    if (oct > 255 || oct < 0) {
+      isValid = false
+    }
+  })
+
+  console.log(octetsArr)
+
+  return isValid
+}
+
+module.exports = isValidIPv4
