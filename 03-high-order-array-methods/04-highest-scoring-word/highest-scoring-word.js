e3a4d25ab25ad9683b5cb1ff1a76741a02a9ff30
Xavi Guasch <xaviguasch@gmail.com>
2023-09-11 17:41:04 +0200

    added solution to 23-highest-scoring-word challenge

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,48 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  const alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ]
 
-module.exports = highestScoringWord;
+  const wordsArr = str.split(' ')
+
+  const wordsByScores = wordsArr.map((word) =>
+    word.split('').reduce((acc, currV) => acc + (alphabet.indexOf(currV) + 1), 0)
+  )
+
+  const biggestScore = wordsByScores.reduce((acc, currV) => {
+    if (currV > acc) {
+      acc = currV
+    }
+
+    return acc
+  }, 0)
+
+  return wordsArr[wordsByScores.indexOf(biggestScore)]
+}
+
+module.exports = highestScoringWord
