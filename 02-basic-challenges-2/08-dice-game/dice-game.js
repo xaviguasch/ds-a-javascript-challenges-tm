114b4d72c09a96f0c45f8d8fc7f675fadd9a7947
Xavi Guasch <xaviguasch@gmail.com>
2023-09-11 13:15:38 +0200

    added solution to 18-dice-game challenge

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,40 @@
-function diceGameSimulation() {}
+function rollDice() {
+  const randomNum = Math.floor(Math.random() * 6) + 1
 
-module.exports = diceGameSimulation;
+  return randomNum
+}
+
+function calculateResult(sum) {
+  if (sum === 7 || sum === 11) {
+    return 'win'
+  } else if (sum === 2 || sum === 3 || sum === 12) {
+    return 'lose'
+  } else {
+    return 'roll again'
+  }
+}
+
+function diceGameSimulation(numSimul) {
+  const resultsArr = []
+
+  for (let i = 0; i < numSimul; i++) {
+    const dice1 = rollDice()
+
+    const dice2 = rollDice()
+
+    const sum = dice1 + dice2
+
+    const newSimul = {
+      dice1,
+      dice2,
+      sum,
+      result: calculateResult(sum),
+    }
+
+    resultsArr.push(newSimul)
+  }
+
+  return resultsArr
+}
+
+module.exports = diceGameSimulation
