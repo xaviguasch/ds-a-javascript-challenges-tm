0df60a04295ca708008aaad7f53ad0b095ab0c46
Xavi Guasch <xaviguasch@gmail.com>
2023-09-11 17:50:41 +0200

    added solution to 24-valid-anagrams challenge

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,25 @@
-function validAnagrams() {}
+function validAnagrams(word1, word2) {
+  const splitWord1 = word1.split('')
+  const splitWord2 = word2.split('')
 
-module.exports = validAnagrams;
+  const hashObj1 = {}
+  const hashObj2 = {}
+
+  splitWord1.forEach((letter) =>
+    hashObj1[letter] ? (hashObj1[letter] += 1) : (hashObj1[letter] = 1)
+  )
+
+  splitWord2.forEach((letter) =>
+    hashObj2[letter] ? (hashObj2[letter] += 1) : (hashObj2[letter] = 1)
+  )
+
+  for (const key in hashObj1) {
+    if (hashObj1[key] !== hashObj2[key]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+module.exports = validAnagrams
