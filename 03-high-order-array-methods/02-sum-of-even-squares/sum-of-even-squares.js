eb7c84fc293b17945e2471897845a83c562cb40f
Xavi Guasch <xaviguasch@gmail.com>
2023-09-11 16:22:56 +0200

    added solution to 21-sum-of-even-squares challenge

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,8 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+  return arr
+    .filter((num) => num % 2 === 0)
+    .map((num) => Math.pow(num, 2))
+    .reduce((acc, currV) => acc + currV, 0)
+}
 
-module.exports = sumOfEvenSquares;
+module.exports = sumOfEvenSquares
