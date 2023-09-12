7ee62e6af657b3cfeeb990a7bf91171f62dbe742
Xavi Guasch <xaviguasch@gmail.com>
2023-09-12 09:10:23 +0200

    added solution to 27-analyze-car-mileage challenge

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,27 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  return cars.reduce(
+    (acc, currV) => {
+      acc.averageMileage = acc.averageMileage + currV.mileage / cars.length
 
-module.exports = analyzeCarMileage;
+      if (currV.mileage > acc.highestMileageCar.mileage) {
+        acc.highestMileageCar = currV
+      }
+
+      if (currV.mileage < acc.lowestMileageCar.mileage) {
+        acc.lowestMileageCar = currV
+      }
+
+      acc.totalMileage = acc.totalMileage + currV.mileage
+
+      return acc
+    },
+    {
+      averageMileage: 0,
+      highestMileageCar: cars[0],
+      lowestMileageCar: cars[0],
+      totalMileage: 0,
+    }
+  )
+}
+
+module.exports = analyzeCarMileage
--- pending-to-revisit.md
+++ pending-to-revisit.md
@@ -1,2 +1,3 @@
 17-first-non-repeating FAILED AT FIRST TRY, TRY AGAIN!!!
 20-validate-email FAILED AT FIRST TRY, TRY AGAIN!!! both regex and normal solutions
+27-valid-ipv4 DID IT RIGHT, but solution was to convoluted. Try again
