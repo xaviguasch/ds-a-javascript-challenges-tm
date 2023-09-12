64a3c7a89d13d0aff103de2cea26ed0bef12f528
Xavi Guasch <xaviguasch@gmail.com>
2023-09-12 11:24:12 +0200

    added solution for 28-password-validator challenge

--- 03-high-order-array-methods/09-password-validator/password-validator-run.js
+++ 03-high-order-array-methods/09-password-validator/password-validator-run.js
@@ -1,6 +1,6 @@
-const validatePassword = require('./password-validator');
+const validatePassword = require('./password-validator')
 
-const result1 = validatePassword('Abc12345');
-const result2 = validatePassword('password');
+const result1 = validatePassword('Abc12345')
+const result2 = validatePassword('password')
 
-console.log(result1, result2);
+console.log(result1, result2)
--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,43 @@
-function validatePassword() {}
+function validatePassword(password) {
+  if (password.length < 8) {
+    return false
+  }
 
-module.exports = validatePassword;
+  const splitPass = password.split('')
+  const nums = []
+  const letters = []
+
+  const separateNumsAndLetters = splitPass.forEach((char) => {
+    const charToNum = Number(char)
+
+    const charStr = charToNum.toString()
+
+    console.log(char)
+
+    if (char === charStr) {
+      nums.push(char)
+    } else {
+      letters.push(char)
+    }
+  })
+
+  const checkForUpperCase = letters.some((char) => {
+    const upperCasedChar = char.toUpperCase()
+
+    if (char === upperCasedChar) {
+      return true
+    }
+  })
+
+  const checkForLowerCase = letters.some((char) => {
+    const lowerCasedChar = char.toLowerCase()
+
+    if (char === lowerCasedChar) {
+      return true
+    }
+  })
+
+  return nums.length > 0 && checkForLowerCase && checkForUpperCase
+}
+
+module.exports = validatePassword
