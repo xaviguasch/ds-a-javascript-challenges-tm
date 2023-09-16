af0bb46eb9a8222dff675ad441fa3d0c91ec2da8
Xavi Guasch <xaviguasch@gmail.com>
2023-09-16 11:29:22 +0200

    added correction to wrong tests from previous challenge 38-max-subarray-quadratic

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,24 +1,20 @@
 function maxSubarraySum(arr, k) {
-  // Initialize a variable 'maxSum' to store the maximum sum of subarrays
-  let maxSum = -Infinity
+  // Initialize maxSum to the smallest possible number
+  let maxSum = Number.NEGATIVE_INFINITY
 
-  // Iterate through the array from index 0 to (length - k)
-  // We only need to consider subarrays of length 'k', so we stop when there's not enough elements left
+  // Iterate through the array
   for (let i = 0; i <= arr.length - k; i++) {
-    // Initialize a variable 'currentSum' to store the sum of the current subarray
     let currentSum = 0
 
-    // Nested loop: Iterate through the subarray of length 'k' starting from index 'i'. This will loop through the current windows
+    // Calculate the sum of the subarray of length k starting at index i
     for (let j = i; j < i + k; j++) {
-      // Add the value at index 'j' to 'currentSum'
       currentSum += arr[j]
     }
 
-    // Update 'maxSum' to be the maximum value between 'maxSum' and 'currentSum'
+    // Update maxSum to the maximum value
     maxSum = Math.max(maxSum, currentSum)
   }
 
-  // Return the maximum sum of any subarray of length 'k'
   return maxSum
 }
 
